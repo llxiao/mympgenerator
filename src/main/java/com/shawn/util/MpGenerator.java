ab50d1d25a3b9c0c677b7eb03e69435ea1f92bef
李晓 <lxiao0126@163.com>
2017-09-14 17:18:11 +0800

    FIX:修改mybaitis-plus 生成模板，使其更符合项目开发使用

--- src/main/java/com/shawn/util/MpGenerator.java
+++ src/main/java/com/shawn/util/MpGenerator.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
@@ -50,7 +51,7 @@ public class MpGenerator {
         //strategy.setTablePrefix(new String[] { "tlog_", "tsys_" });// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
         strategy.setFieldNaming(NamingStrategy.underline_to_camel);
-        strategy.setInclude(new String[]{"fs_own_share_group_info"}); // 需要生成的表
+        strategy.setInclude(new String[]{"fs_share_send_info"}); // 需要生成的表
 
         // strategy.setExclude(new String[]{"test"}); // 排除生成的表
         // 自定义实体父类
@@ -79,9 +80,16 @@ public class MpGenerator {
         pc.setParent("com.hangtuo");
         pc.setController("controller");
         pc.setEntity("entity");
-        pc.setMapper("mapper");
+        pc.setMapper("persistence");
         pc.setService("service");
         mpg.setPackageInfo(pc);
+
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
+        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+         TemplateConfig tc = new TemplateConfig();
+
+        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
+        mpg.setTemplate(tc);
 
 
         // 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
