a666ab5c990b1b19ff3d460be7fd1ee62b07f04b
李晓 <lxiao0126@163.com>
2018-01-12 14:50:18 +0800

    FIX:修改生成

--- src/main/java/com/shawn/util/MpGenerator.java
+++ src/main/java/com/shawn/util/MpGenerator.java
@@ -8,9 +8,12 @@ import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableField;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.xiaoleilu.hutool.util.CollectionUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,21 +27,22 @@ public class MpGenerator {
         AutoGenerator mpg = new AutoGenerator();
         // 全局配置ActiveDemo
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("D://develop");   //生成的目录的地址
+        //生成的目录的地址
+        gc.setOutputDir("D://develop");
         gc.setFileOverride(true);
         gc.setActiveRecord(true);
-        gc.setEnableCache(false);// XML 二级缓存
-        gc.setBaseResultMap(true);// XML ResultMap
-        gc.setBaseColumnList(true);// XML columList
-        gc.setAuthor("Shawn");        //作者名称
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(true);
+        // XML columList
+        gc.setBaseColumnList(true);
 
         // 自定义文件命名，注意 %s 会自动填充表实体属性！
         gc.setMapperName("%sMapper");
         gc.setXmlName("%sMapper");
         gc.setServiceName("%sService");
         gc.setServiceImplName("%sServiceImpl");
-//        gc.setControllerName("%sController");
-
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -47,13 +51,19 @@ public class MpGenerator {
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("ycc123456");
-        dsc.setUrl("jdbc:mysql://192.168.1.122:3306/gouliao?characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://192.168.1.122:3306/msg?characterEncoding=utf8");
         mpg.setDataSource(dsc);
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
-        // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
-        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        strategy.setInclude(new String[]{"fs_share_send_info"}); // 需要生成的表
+        // 全局大写命名 ORACLE 注意
+        // strategy.setCapitalMode(true);
+        // 表名生成策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        // 需要生成的表
+//        strategy.setInclude(new String[]{"group_apply_info"});
+        strategy.setEntityLombokModel(true);
+
+
 
         // strategy.setExclude(new String[]{"test"}); // 排除生成的表
         // 自定义实体父类
@@ -73,17 +83,19 @@ public class MpGenerator {
         // strategy.setEntityColumnConstant(true);
         // 【实体】是否为构建者模型（默认 false）
         // public User setName(String name) {this.name = name; return this;}
+        //strategy.setEntityBuliderModel(true);
 
-         //strategy.setEntityBuliderModel(true);
         mpg.setStrategy(strategy);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setParent("com.hangtuo");
-//        pc.setController("controller");
-        pc.setEntity("entity");
-        pc.setMapper("persistence");
-        pc.setService("service");
+        pc.setParent("cn.gouliao.ares");
+        pc.setEntity("entity.group.newbase");
+        pc.setMapper("mapper");
+        pc.setService("service.group");
+        pc.setXml("mapper");
+        pc.setServiceImpl("group.impl");
+
         mpg.setPackageInfo(pc);
 
 
@@ -91,10 +103,9 @@ public class MpGenerator {
         // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
         // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
         TemplateConfig tc = new TemplateConfig();
-
         // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
+        tc.setController(null);
         mpg.setTemplate(tc);
-
 
         // 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
         InjectionConfig cfg = new InjectionConfig() {
